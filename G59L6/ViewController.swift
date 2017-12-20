f9acbb57b191feae42ecd1be7a17f153bac1ce66
Ivan Besarab <Ivan.Besarab@RockSoft.rocks>
2017-12-20 09:49:31 +0200

    sender argument usage example

--- G59L6/ViewController.swift
+++ G59L6/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
 		let name = "Ivan"
 		let dict = ["Ivan" : "KROSAFCHEG",
 					"a" : "b"]
-//		print(dict[name])
+		//		print(dict[name])
 		titleLabel.text = name + " " + dict[name]!
 	}
 	
@@ -29,15 +29,43 @@ class ViewController: UIViewController {
 	func setTitleText() {
 		titleLabel.text = "LOL"
 	}
-
-
+	
+	
 	@IBAction func buttonPressed() {
-		print(titleLabel.text)
-		//взять строку с titleLabel
-//		перевести ее к типу Int - получить число
-//		добавить к нему +1
-//		перевести число в строку
-//		присвоить число в titleLabel
+		print(titleLabel.text!)
+//		//взять строку с titleLabel
+//		let stringFromLabel = titleLabel.text
+//		//		перевести ее к типу Int - получить число
+//		let intFromLabel = Int(stringFromLabel ?? "0")
+//		//		добавить к нему +1
+//		let result = (intFromLabel ?? 0) + 1
+//		//		перевести число в строку
+//		let stringResult = String(result)//result.description//"\(result)"
+//		//		присвоить число в titleLabel
+//		titleLabel.text = stringResult
+		
+		titleLabel.text = ((Int(titleLabel.text ?? "0") ?? 0) + 1).description
+	}
+	
+	@IBAction func digitButtonPressed(_ sender: UIButton) {
+		print(sender.titleLabel?.text)
+		print(self.titleLabel?.text)
+	}
+	
+	
+	@IBAction func counterChange(_ sender: UIButton) {
+//		var a = 0
+//		if sender.tag == 0 {
+//			print("-1")
+//			a = -1
+//		}
+//		else {
+//			print("+1")
+//			a = +1
+//		}
+		sender.tag == 0 ? print("-1") : print("+1")
+		let a = sender.tag == 0 ? -1 : +1
+		titleLabel.text = ((Int(titleLabel.text ?? "0") ?? 0) + a).description
 	}
 	
 }
